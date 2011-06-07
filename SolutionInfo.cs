3200687ec4a320fba986fee939654fb6abdaaac9
joelabrahamsson <mail@joelabrahamsson.com>
2011-06-07 16:27:13 +0200

    Added shared assemblyinfo

---
+++ SolutionInfo.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyCopyright("Copyright © Joel Abrahamsson")]
+
+#if (DEBUG)
+[assembly: AssemblyConfiguration("Debug")]
+#else
+[assembly: AssemblyConfiguration("Release")]
+#endif
+
+[assembly: ComVisible(false)]
+
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
