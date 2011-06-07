ea3d188365a8ed198667fe9bca304bbc94ba84d0
joelabrahamsson <mail@joelabrahamsson.com>
2011-06-07 17:27:01 +0200

    Added TypeExtensions and signing

---
+++ Refraction/TypeExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Reflection;
+
+namespace Refraction
+{
+    public static class TypeExtensions
+    {
+        public static TValue InvokeMember<TValue>(this Type type, string name, BindingFlags bindingFlags, params object[] arguments)
+        {
+            return (TValue)type.InvokeMember(name, bindingFlags, null, null, arguments);
+        }
+
+        public static void InvokeMember(this Type type, string name, BindingFlags bindingFlags, params object[] arguments)
+        {
+            type.InvokeMember(name, bindingFlags, null, null, arguments);
+        }
+    }
+}
